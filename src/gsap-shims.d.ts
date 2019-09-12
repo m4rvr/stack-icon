5eb16c585e5e813e815efd3e186d0a32109cd9db
Marvin Rudolph <info@marvin-rudolph.de>
2019-09-12 10:45:26 +0200

    :rotating_light: Added semicolon to make eslint/prettier happy

--- src/gsap-shims.d.ts
+++ src/gsap-shims.d.ts
@@ -3,6 +3,6 @@
  * So this file fixes the type-check until it will be fixed by `@types/gsap` itself
  */
 declare module 'gsap/all' {
-  import gsap from 'gsap'
-  export = gsap
+  import gsap from 'gsap';
+  export = gsap;
 }
