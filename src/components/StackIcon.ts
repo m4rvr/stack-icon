7fe7e3146721a52de69a019ecfe8c85b8ee16494
Marvin Rudolph <info@marvin-rudolph.de>
2019-09-12 15:49:05 +0200

    :sparkles: First stack animation tests

--- src/components/StackIcon.ts
+++ src/components/StackIcon.ts
@@ -6,19 +6,42 @@ enum ElementPosition {
   Bottom,
 }
 
+interface Options {
+  start: number;
+  min: number;
+  max: number;
+}
+
 class StackIcon {
-  protected element: HTMLElement;
+  protected element!: HTMLElement;
   protected readonly elementSpacing: number = 5;
-  protected topElement: SVGPolygonElement | null;
-  protected stackItems: NodeListOf<SVGPolylineElement> | null;
-  protected stackItemsIndicator: HTMLElement | null;
-  protected decrementButton: HTMLElement | null;
-  protected incrementButton: HTMLElement | null;
+  protected svgElement!: SVGElement | null;
+  protected topElement!: SVGPolygonElement | null;
+  protected topAnimationElement!: SVGPolygonElement | null;
+  protected stackItems!: NodeListOf<SVGPolylineElement> | null;
+  protected stackItemsIndicator!: HTMLElement | null;
+  protected decrementButton!: HTMLElement | null;
+  protected incrementButton!: HTMLElement | null;
   protected numberOfItems = 1;
+  protected defaultOptions: Options = {
+    start: 1,
+    min: 1,
+    max: 99,
+  };
+
+  protected options: Options = {
+    ...this.defaultOptions,
+  };
 
   constructor(element: HTMLElement) {
     this.element = element;
+    this.svgElement = element.querySelector('svg');
     this.topElement = element.querySelector('polygon');
+    if (this.topElement) {
+      this.topAnimationElement = this.topElement.cloneNode(
+        false,
+      ) as SVGPolygonElement;
+    }
     this.stackItems = element.querySelectorAll('polyline');
     this.stackItemsIndicator = element.querySelector('[data-stack-items]');
     this.decrementButton = element.querySelector('[data-decrement]');
@@ -26,14 +49,15 @@ class StackIcon {
   }
 
   public initialize(): void {
-    this.setTopElementPosition(ElementPosition.Bottom);
-    this.bindEventListener();
-    this.updateStackItemsNumber();
-    this.show();
+    this.setOptions();
+    this.initializeStack();
   }
 
   public decrement(): void {
-    if (this.numberOfItems - 1 > 0) {
+    if (
+      this.numberOfItems - 1 > 0 &&
+      this.numberOfItems - 1 >= this.options.min
+    ) {
       this.numberOfItems = this.numberOfItems - 1;
       this.updateStackItemsNumber();
 
@@ -44,9 +68,57 @@ class StackIcon {
   }
 
   public increment(): void {
-    this.numberOfItems = this.numberOfItems + 1;
-    this.handleStackAnimation();
+    if (this.numberOfItems + 1 <= this.options.max || this.options.max === 0) {
+      this.numberOfItems = this.numberOfItems + 1;
+      this.handleStackAnimation();
+      this.updateStackItemsNumber();
+    }
+  }
+
+  protected setOptions(): void {
+    if (this.element.dataset.stack) {
+      this.options.start = Math.max(
+        Math.ceil(Number(this.element.dataset.stack)),
+        1,
+      );
+    }
+
+    if (this.element.dataset.min) {
+      const min = Math.ceil(Number(this.element.dataset.min));
+
+      if (min >= 1) {
+        this.options.min = min;
+      } else {
+        console.warn(
+          `[Stack Icon] data-min is lower than 1, fallback to default ${this.defaultOptions.min}`,
+        );
+      }
+    }
+
+    if (this.element.dataset.max) {
+      const max = Math.ceil(Number(this.element.dataset.max));
+
+      if (max >= 0) {
+        this.options.max = max;
+      } else {
+        const defaultMax =
+          this.defaultOptions.max === 0
+            ? `${this.defaultOptions.max} (infinite)`
+            : this.defaultOptions.max;
+
+        console.warn(
+          `[Stack Icon] data-max is lower than 0, fallback to default ${defaultMax}`,
+        );
+      }
+    }
+  }
+
+  protected initializeStack(): void {
+    this.numberOfItems = this.options.start;
+    this.setTopElementPosition(ElementPosition.Bottom);
+    this.bindEventListener();
     this.updateStackItemsNumber();
+    this.show();
   }
 
   protected setTopElementPosition(position: ElementPosition): void {
@@ -109,12 +181,53 @@ class StackIcon {
         break;
 
       case 2:
-        console.log('show 2!');
+        this.testAnimation();
         break;
 
       case 3:
         console.log('show 3!');
         break;
+    }
+  }
+
+  protected testAnimation(): void {
+    if (
+      this.svgElement &&
+      this.topElement &&
+      this.topAnimationElement &&
+      this.stackItems
+    ) {
+      const timeline = new TimelineLite({
+        ease: Elastic.easeInOut,
+      });
+
+      this.svgElement.prepend(this.topAnimationElement);
+
+      timeline.set(this.topAnimationElement, {
+        autoAlpha: 0,
+      });
+
+      timeline.to(this.topAnimationElement, 0.3, {
+        autoAlpha: 1,
+        y: 10,
+      });
+
+      timeline.set(this.topAnimationElement, {
+        autoAlpha: 0,
+      });
+
+      timeline.to(this.stackItems, 0.3, {
+        autoAlpha: 1,
+      });
+
+      timeline.to(
+        this.topElement,
+        0.3,
+        {
+          y: 0,
+        },
+        '-=0.3',
+      );
     }
   }
 }
